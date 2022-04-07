59dc3ff0f22b95e50fc5d19c1f55aca7d9630c8f
Andreislusari <Andrei.slusari.2020@gmail.com>
2022-04-07 19:50:41 +0300

    init

---
+++ src/config/cloudConfig.js
@@ -0,0 +1,15 @@
+/**
+ * The cloud name of the Cloudinary account used throughout the application.
+ * Replace <cloudName> with the name of your cloud.
+ * @type {string}
+ */
+export const cloudName = "dhnjtro9x";
+
+/**
+ * The unsigned upload preset used for unsigned uploads, using the upload widget.
+ * Replace <uploadPreset> with the name of your unsigned upload preset.
+ * @type {string}
+ */
+export const uploadPreset = "rxwqi2xs";
+
+export default {cloudName, uploadPreset};
